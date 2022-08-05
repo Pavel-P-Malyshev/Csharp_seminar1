f6a40d362d959c7f66ca5dc108ae10b9986ed831
Pavel <m83819@gmail.com>
2022-08-05 11:56:51 +0700

    Task006_abs

---
+++ Task006_abs/Program.cs
@@ -0,0 +1,18 @@
+﻿/*. Напишите программу вычисления модуля числа.
+2 -> 2
+-3 -> 3
+-7 -> 7
+**. Напишите программу, которая на вход принимает значение, а 
+на выходе показывает обратное значение.
+1 -> 1 
+2 -> 0.5
+0.25 -> 4
+
+*/
+Console.WriteLine("Enter any integer number:");
+int N = int.Parse(Console.ReadLine()); //преобразование строки в целове число
+
+
+Console.WriteLine($"For you number the absolute value is {Math.Abs(N)}");
+Console.WriteLine($" The -1 power  of your number is {(double)1/N}");
+
