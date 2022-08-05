7b7baf5200fb2cf5afebea687059da616fd06e38
Pavel <m83819@gmail.com>
2022-08-05 11:06:01 +0700

    Task003_MapWeekday

---
+++ Task003_MapWeekday/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая будет выдавать 
+название дня недели по заданному номеру.
+3 -> Среда 
+5 -> Пятница
+*/
+Console.WriteLine("Enter number of a week day from 1 to 7");
+int wd = int.Parse(Console.ReadLine()); //преобразование строки в целове число
+string[] wd_name={"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+
+Console.Write ($"Your day of week is called '{wd_name[wd-1]}'");
+
+
