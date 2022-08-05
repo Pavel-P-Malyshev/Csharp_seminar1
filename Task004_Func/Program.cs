12b2045594805466ad4677df53c190b019cfbee6
Pavel <m83819@gmail.com>
2022-08-05 11:32:30 +0700

    Task004_Func

---
+++ Task004_Func/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу вычисления функции:
+ x = f(a)*/
+
+ int F(int a)
+ {
+    return a*a*a-2*a*a+1;
+ }
+
+Console.WriteLine("Enter argument range: 'a' starts from>");
+int a1 = int.Parse(Console.ReadLine()); //преобразование строки в целове число
+Console.WriteLine("Enter argument range: 'a' ends with>");
+int a2 = int.Parse(Console.ReadLine()); //преобразование строки в целове число
+
+int i;
+for(i=a1;i<=a2;i++)
+{
+    System.Console.WriteLine($"for a={i} x=F(a) is {F(i)}");
+}
